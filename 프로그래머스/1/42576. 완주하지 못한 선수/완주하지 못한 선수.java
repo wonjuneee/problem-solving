85b04ba1020604570291ba28de5ba76f7dc2790b
WonJun Lee <tony000709@gmail.com>
2026-07-23 04:19:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.06 ms, Memory: 122 MB -BaekjoonHub
    - 이전 `Map`, `compute`을 사용한 방식도 반드시 참고할 것
      - `compute`의 인자로 반환값이 null인 `remappingFunction`을 넘겨줄 경우 해당 key의 노드는 삭제된다.

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,31 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public static String solution(String[] participant, String[] completion) { 
 
-        Map<String, Integer> participantMap = new HashMap<>(); 
+        /**
+        *   participant와 completion에 쌍으로 존재하는 참가자는 Set에서 지워지고
+        *   그렇지 않은, 이 문제에서는 참가했지만 완주하지 못한, 참가자만 Set에 남게 된다.
+        *   결과적 일관성으로 participant, completion 원소 순서 상관없이 두 배열을 합쳐 짝수번 등장할 때 Set에서 원소를 제거한다.
+        */
         
-        for (int i = 0; i < participant.length ; i++) { 
-            participantMap.compute(participant[i], (k, v) -> v != null ? null : 1);
+        Set<String> participantSet = new HashSet<>(); 
+        
+        for (int i = 0; i < participant.length ; i++) {
+            // Set에 원소가 존재하지 않아 remove에 실패한 경우, Set에 원소를 추가한다.
+            // 그렇지 않은 경우 remove만 진행한다.
+            if (!participantSet.remove(participant[i])) {
+                participantSet.add(participant[i]);
+            }
             
             if (i < completion.length) {
-                participantMap.compute(completion[i], (k,v) -> v != null ? null : 1);
+                if (!participantSet.remove(completion[i])) {
+                    participantSet.add(completion[i]);
+                }
             }
         }
         
-        return participantMap.keySet().iterator().next(); 
+        return participantSet.iterator().next(); 
     }
 }
