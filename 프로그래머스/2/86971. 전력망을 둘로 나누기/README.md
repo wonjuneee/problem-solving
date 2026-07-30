03e432dfbb42df46fe1a578cede476bc37d46571
WonJun Lee <tony000709@gmail.com>
2026-07-30 16:45:00 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.29 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
 
 ### 성능 요약
 
-메모리: 84.8 MB, 시간: 0.22 ms
+메모리: 87.8 MB, 시간: 2.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 30일 01:40:59
+2026년 07월 30일 16:44:52
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,59 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = -1;
+        int answer = n;
         
         /**
-        *   양방향 간선에 대해 DFS로 순회하여 각 노드가 가지는 본인 포함 자식 노드 개수를 판단한다.
-        *   각 DFS 순회에서 전체 노드 개수 total에서 한 자식노드를 루트로 한 네트워크의 노드 개수 childCnt를 뺀 값이 현재 node의 송전탑 개수이고,
-        *   childCnt가 해당 자식노드가 루트인 네트워크의 송전탑 개수이다.
-        *   이로부터 두 네트워크의 송전탑 개수 차이 diff는 Math.abs(total - (childCnt * 2))가 된다.
-        *   이것을 모든 노드에서, 각 노드의 자식노드를 분리하여 2개의 네트워크로 만들었을 때의 diff 최소값을 최종적으로 반환한다.
+        *   Union-Find로 주어진 wires 중 하나를 연결하지 않아 2개의 네트워크로 구분한다.
+        *   각각의 연결되지 않은 wire 케이스에 대해, 송전탑 개수 차이의 최소값을 반환한다.
         */
         
-        List<Integer>[] tree = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            tree[i] = new ArrayList<>();
+        int[] root = new int[n + 1];
+        int[] rank = new int[n + 1];
+
+        for (int i = 0; i < wires.length; i++) {
+            
+            for (int j = 1; j <= n; j++) {
+                root[j] = j;
+                rank[j] = 1;
+            }
+            
+            for (int j = 0; j < wires.length; j++) {
+                if (i == j) {
+                    continue;
+                }   
+                union(root, rank, wires[j][0], wires[j][1]);   
+            }
+            answer = Math.min(answer, Math.abs(n - rank[find(root, 1)] * 2));            
         }
-        for (int[] wire: wires) {
-            tree[wire[0]].add(wire[1]);
-            tree[wire[1]].add(wire[0]);
-        }
-        // System.out.println(tree);
-        
-        boolean[] isVisited = new boolean[n + 1];
-        
-        answer = dfs(tree, isVisited, 1, n)[1];
         
         return answer;
     }
     
-    int[] dfs(List<Integer>[] tree, boolean[] isVisited, int node, int n) {        
+    void union(int[] root, int[] rank, int a, int b) {
+        int rootA = find(root, a);
+        int rootB = find(root, b);
         
-        // 부모 노드에 반환할 자식 노드 개수에 자신도 포함
-        int cnt = 1, diff = n - 1;
-        isVisited[node] = true;
-        
-        for (Integer child: tree[node]) {
-            // 양방향 간선으로 구성되어 있으므로, 말단 노드는 이미 방문한 노드들과만 연결되어 있다고 볼 수 있다. 따라서 { cnt=1, diff=n }을 반환한다.
-            if (!isVisited[child]) {
-                
-                int[] childInfo = dfs(tree, isVisited, child, n);
-                int childCnt = childInfo[0], childDiff = childInfo[1];
-
-                // 현재 node에서 한 자식노드에 대한 diff는 n에서 childCnt를 뺀 값과 childCnt를 뺀 값이, 두 그래프의 노드 개수 차이이다.
-                // 이때 현재 node의 최종 diff는 모든 자식노드에 대한 diff 중 최소값이다.
-                diff = Math.min(diff, Math.min(childDiff, Math.abs(n - (childCnt * 2))));
-                cnt += childCnt;
-                
-                // System.out.println("NODE: " + node + ", DIFF: " +  Math.abs(n - (childCnt * 2)));
-            }
+        if (rootA == rootB) {
+            return;
         }
         
-        //System.out.println("NODE: " + node + "(" + cnt + ")" + ", DIFF: " + diff);
+        if (rank[rootA] <= rank[rootB]) {
+            root[rootA] = rootB;
+            rank[rootB] += rank[rootA];
+        } else {
+            root[rootB] = rootA;
+            rank[rootA] += rank[rootB];
+        }
+    }
+    
+    int find(int[] root, int a) {
+        if (root[a] == a) {
+            return a;
+        }
         
-        return new int[]{ cnt, diff };
+        root[a] = find(root, root[a]);
+        return root[a];
     }
 }
