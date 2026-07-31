88f58930c54b0c9fd537d2521d4a48afa40aa08c
WonJun Lee <tony000709@gmail.com>
2026-08-01 03:31:53 +0900

    [level 3] Title: 정수 삼각형, Time: 5.07 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -5,18 +5,16 @@ class Solution {
         /**
         *   모든 케이스를 계산하는 경우, 최대 2^500개의 케이스를 계산해야 한다.
         *   따라서 DP 테이블을 활용해 연산 횟수를 최소화할 수 있다.
+        *   - 기저 조건(삼각형 최하단)부터 값을 더해가는 바텀업 방식의 DP로 구현
+        *   - 별도의 DP 테이블 대신, triangle 배열을 그대로 사용할 수 있다.
         */
-        
-        int[][] dp = new int[triangle.length][triangle.length];
-        
-        System.arraycopy(triangle[triangle.length - 1], 0, dp[triangle.length - 1], 0, triangle.length);
         
         for (int i = triangle.length - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                dp[i][j] = triangle[i][j] + Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
+                triangle[i][j] = triangle[i][j] + Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
-        answer = dp[0][0];
+        answer = triangle[0][0];
         
         return answer;
     }
