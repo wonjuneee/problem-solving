1dc8243eaca92ef0394781589aa5bacb36e26a27
WonJun Lee <tony000709@gmail.com>
2026-08-01 16:38:34 +0900

    [level 2] Title: 튜플, Time: 33.38 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -4,19 +4,20 @@ import java.util.Set;
 
 class Solution {
     public int[] solution(String s) {
+        /**
+        *   중복되는 원소가 없는 튜플이므로, 한 번 등장한 숫자는 해시셋에 넣어 중복여부를 판단할 수 있다.
+        *   Input으로 주어진 문자열은 "\\},\\{"로 파싱한 뒤, 길이에 대한 오름차순으로 정렬한다.
+        *   파싱된 집합에 대한 배열을 순회하여, 해시셋에 대한 중복 체크를 하여 해시셋에 존재하지 않는 값을 즉시 반환한다.
+        */
+        
+        
         String[] set = s.substring(2, s.length() - 2).split("\\},\\{");
         Arrays.sort(set, (s1, s2) -> s1.length() - s2.length());
         int[] answer = new int[set.length];
         
-        // System.out.println(Arrays.toString(set));
-        // if (set.length > 2) {
-        //     System.out.println(set[1]);
-        // }
-        
         Set<Integer> placedSet = new HashSet<>();
         for (int i = 0; i < set.length; i++) {
             int[] parsed = parseSetIntoArray(set[i]);
-            // System.out.println(Arrays.toString(parsed));
             
             int number = Arrays.stream(parsed)
                 .filter(num -> !placedSet.contains(num))
