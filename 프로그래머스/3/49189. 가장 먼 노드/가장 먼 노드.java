087522e906eea893b60a140bff3dc2a89ce9de92
WonJun Lee <tony000709@gmail.com>
2026-08-07 20:51:10 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.21 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,7 +1,8 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.List;
+import java.util.Queue;
 
 class Solution {
     public int solution(int n, int[][] edge) {
@@ -23,14 +24,9 @@ class Solution {
             map[e[1]].add(e[0]);
         }
         
-        dijkstra(map, cost);
-        
-        int max = 0;
+        int max = bfs(map, cost);
         for (int i = 2; i <= n; i++) {
-            if (max < cost[i]) {
-                answer = 1;
-                max = cost[i];
-            } else if (max == cost[i]) {
+            if (max == cost[i]) {
                 answer++;
             }
         }
@@ -38,24 +34,26 @@ class Solution {
         return answer;
     }
     
-    void dijkstra(List<Integer>[] map, int[] cost) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> i1[1] - i2[1]);
-        pq.add(new int[]{ 1, 0 });
+    int bfs(List<Integer>[] map, int[] cost) {
+        Queue<Integer> q = new ArrayDeque<>();
+        cost[1] = 0;
+        q.add(1);
         
-        while (!pq.isEmpty()) {
-            int[] currNode = pq.poll();
-            int currValue = currNode[0], currCost = currNode[1];
+        int max = 0;
+        while (!q.isEmpty()) {
+            int currValue = q.poll();
             
-            if (currCost < cost[currValue]) {
-                cost[currValue] = currCost;
-                
-                for (int nextValue: map[currValue]) {
-                    int nextCost = cost[nextValue];
-                    if (nextCost > currCost + 1) {
-                        pq.add(new int[]{ nextValue, currCost + 1 });
-                    }
+            for (int nextValue: map[currValue]) {
+                if (cost[nextValue] == Integer.MAX_VALUE) {
+                    int nextCost = cost[currValue] + 1;
+                    cost[nextValue] = nextCost;
+                    q.add(nextValue);
+                    
+                    max = Math.max(nextCost, max);
                 }
             }
         }
+        
+        return max;
     }
 }
