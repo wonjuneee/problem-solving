7bdf6f666e46ae46eda2c9a380174418b18330ca
WonJun Lee <tony000709@gmail.com>
2026-08-13 21:03:30 +0900

    [level 2] Title: 전화번호 목록, Time: 647.03 ms, Memory: 588 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,15 +7,17 @@ class Solution {
         boolean answer = true;
         /**
         *   Trie 구조를 사용해 각 문자열의 prefix를 확인할 수 있다.
+        *   문자열이 Trie에 모두 저장되면 한 문자열이 종료됨을 의미하는 isEnd 필드의 상태값을 true로 변경한다.
+        *   이후 문자열 탐색 중, 주어진 문자열을 모두 탐색하기 전에 isEnd 필드가 true인 노드를 만나면 해당 문자열의 prefix인 어떤 문자열이 존재함을 의미한다.
         */
         
         Trie root = new Trie();
         for (String phoneNumb: phone_book) {
-            buildTrie(phoneNumb.toCharArray(), root);
+            root.insert(phoneNumb);
         }
         
         for (String phoneNumb: phone_book) {
-            answer = search(phoneNumb.toCharArray(), root);
+            answer = root.search(phoneNumb);
             
             if (!answer) {
                 break;
@@ -25,39 +27,40 @@ class Solution {
         return answer;
     }
     
-    class Trie {
-        Map<Character, Trie> child;
+    class Node {
+        Map<Character, Node> child = new HashMap<>();
         boolean isEnd = false;
-        
-        public Trie() {
-            this.child = new HashMap<>();
-        }
     }
     
-    public void buildTrie(char[] charArr, Trie node) {
-        if (charArr.length == 0) {            
-            return;
-        }
-        node.child.putIfAbsent(charArr[0], new Trie());
-        Trie next = node.child.get(charArr[0]);
+    class Trie {
+        Node root = new Node();
         
-        buildTrie(Arrays.copyOfRange(charArr, 1, charArr.length), next);
-        
-        if (!next.child.isEmpty()) {
-            next.isEnd = false;
-        } else {
-            next.isEnd = true;
+        public void insert(String str) {
+            Node node = root;
+            
+            for (int i = 0; i < str.length(); i++) {
+                // node.child.putIfAbsent(str.charAt(i), new Node());
+                // node = node.child.get(str.charAt(i));
+                
+                // 새로운 문자면 Node 생성, 이미 존재하는 문자면 기존 Node 그대로 반환 
+                node = node.child.computeIfAbsent(str.charAt(i), k -> new Node());
+            }
+            
+            node.isEnd = true;
         }
-    }
     
-    public boolean search(char[] charArr, Trie node) {
-        if (node.isEnd && charArr.length == 0) {
+        public boolean search(String str) {
+            Node node = root;
+            for (int i = 0; i < str.length(); i++) {
+                node = node.child.get(str.charAt(i));
+                
+                // 현재 탐색 중인 문자열의 prefix에 해당하는 어떤 문자열이 존재
+                if (node.isEnd && i < str.length() - 1) {
+                    return false;
+                }
+            }
+            // 주어진 문자열이 그대로 검색되었으므로, prefix에 해당하는 문자열이 존재하지 않음
             return true;
-        } else if (!node.isEnd && charArr.length == 0) {
-            return false;
         }
-        
-        Trie next = node.child.get(charArr[0]);
-        return search(Arrays.copyOfRange(charArr, 1, charArr.length), next);
     }
 }
