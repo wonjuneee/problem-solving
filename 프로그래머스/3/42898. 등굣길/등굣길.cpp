4b6fbf1e1857cac38516400745f2b3fe7c150185
WonJun Lee <tony000709@gmail.com>
2026-09-03 18:52:43 +0900

    [level 3] Title: 등굣길, Time: 0.05 ms, Memory: 4.75 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -7,8 +7,8 @@ const int DIV = 1000000007;
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
     /**
-    *   1. 전체 경우의 수에서 웅덩이를 지나 학교에 도착하는 경우의 수 빼기
-    *   2. 한 칸에서 이전 행 / 이전 열에서 오는 경우를 더한다. 이때 웅덩이가 있는 칸은 -1에서 순회 중 0으로 갱신한 뒤, 이후 칸에서 해당 방향의 경우의 수는 0으로 더해지도록 한다.
+    *   한 칸에서 이전 행 / 이전 열에서 오는 경우를 더한다. 
+    *   이때 웅덩이가 있는 칸은 -1에서 순회 중 0으로 갱신한 뒤, 이후 칸에서 해당 방향의 경우의 수는 0으로 더해지도록 한다.
     */
     
     vector<vector<int>> dp(n + 1, vector<int>(m + 1));
@@ -25,10 +25,10 @@ int solution(int m, int n, vector<vector<int>> puddles) {
                 dp[row][col] = 0;
                 continue;
             }
-            dp[row][col] = ((dp[row - 1][col] % DIV) + (dp[row][col - 1] % DIV)) % DIV;
+            dp[row][col] = (dp[row - 1][col] + dp[row][col - 1]) % DIV;
         }
     }
-    answer = dp[n][m] % DIV;
+    answer = dp[n][m];
     
     return answer;
 }
