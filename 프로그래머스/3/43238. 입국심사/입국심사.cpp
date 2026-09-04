ba1577609872d50fbdcfa4d0bdc44db4095ca646
WonJun Lee <tony000709@gmail.com>
2026-09-05 03:45:58 +0900

    [level 3] Title: 입국심사, Time: 11.87 ms, Memory: 7.09 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -7,24 +7,22 @@ long long solution(int n, vector<int> times) {
     /**
     *   N이 10억이므로, O(logN)의 시간복잡도를 가진 로직 혹은 심사관을 기준으로 처리하는 로직을 활용해야 한다.
     *   이분 탐색으로 X((left + right) / 2)초에 N명의 사람을 처리할 수 있는지 여부를 판단한다.
-    *       - 처리 가능하면 right = X - 1초에도 처리 가능한지 판단한다.
+    *       - 범위 내 Lower Bound를 구하므로 left < right 동안 반복문을 수행한다.
+    *       - 처리 가능하면 right = X초에도 처리 가능한지 판단한다.
     *       - 처리 불가능하면 left = X + 1초에는 처리 가능한지 판단한다.
     */
     
-    int minimum = 1'000'000'000;
-    for (int time: times) {
-        minimum = min(time, minimum);
-    }
-    long long min = (long long) n * minimum;
+    long long min = (long long) *min_element(times.begin(), times.end()) * n;
     
     long long left = 0, right = min < 0 ? LLONG_MAX : min;
     while (left < right) {
         long long x = left + (right - left) / 2;
-        cout << x << "(" << left << ", " << right << ")" << '\n';
         
         long long sum = 0;
         for (int time: times) {
             sum += x / time;
+            
+            if (sum >= n) break;
         }
         
         if (sum >= n) {
